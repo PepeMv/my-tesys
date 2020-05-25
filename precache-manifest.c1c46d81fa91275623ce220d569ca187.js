cd269a41d2406db870f44d59ab7d35afc0de3ee5
PepeMv <pepemv1997@gmail.com>
2020-05-25 13:03:31 -0500

    Updates

---
+++ precache-manifest.c1c46d81fa91275623ce220d569ca187.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c976f5dc631a8967f6136040aae3843",
+    "url": "/my-tesys/index.html"
+  },
+  {
+    "revision": "a8b2db5759ed252a7213",
+    "url": "/my-tesys/static/css/main.ae32c838.chunk.css"
+  },
+  {
+    "revision": "06db913c7307894069be",
+    "url": "/my-tesys/static/js/2.2d4d20e2.chunk.js"
+  },
+  {
+    "revision": "9b7ee493f26da550442b2e06b0a89cb8",
+    "url": "/my-tesys/static/js/2.2d4d20e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b2db5759ed252a7213",
+    "url": "/my-tesys/static/js/main.a55dc8d4.chunk.js"
+  },
+  {
+    "revision": "a7d3754b83bac67e424d",
+    "url": "/my-tesys/static/js/runtime-main.43b70463.js"
+  },
+  {
+    "revision": "b3dd894c3bc63638f676804e2647d03c",
+    "url": "/my-tesys/static/media/EncabezadoBackground.b3dd894c.jpg"
+  },
+  {
+    "revision": "33cdd8e75bdbfe5816a562fd7b0d83d0",
+    "url": "/my-tesys/static/media/imagesBackgroun.33cdd8e7.jpg"
+  }
+]);
