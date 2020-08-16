d101a3ec2c5f4a92aa47f3118354756dcc73eba0
PepeMv <pepemv1997@gmail.com>
2020-08-16 16:06:11 -0500

    modifique sockets y axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 
 const clienteAxios = axios.create({
-  baseURL: "http://52.1.160.159/api",
-  headers: {'Access-Control-Allow-Origin' : '*'}
+  baseURL: "https://52.1.160.159/api",
 });
 
 export default clienteAxios;
