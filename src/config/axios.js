235d0a768a6a38e1e743783cfc15510ba65b5f43
PepeMv <pepemv1997@gmail.com>
2020-08-16 15:53:31 -0500

    agrege headers al axios modifique axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const clienteAxios = axios.create({
-  baseURL: "https://52.1.160.159/api",
-  headers: {'Access-Control-Allow-Origin' : "*"}
+  baseURL: "http://52.1.160.159/api",
+  headers: {'Access-Control-Allow-Origin' : '*'}
 });
 
 export default clienteAxios;
